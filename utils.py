e32b1c60001e4ff90b4196313f9f3d69eb5aca8e
Luka Steinbach <luka.steinbach@gmx.de>
2019-11-02 12:48:17 +0100

    Added documentation

--- utils.py
+++ utils.py
@@ -6,14 +6,43 @@ CSGO_FEE = 0.10
 
 
 def getPayoutFromPrice(price):
+    """Returns the payout you get on the steam market for selling an item for
+    the given price.
+    
+    Arguments:
+        price {float} -- The price the buyer pays to buy your item.
+    
+    Returns:
+        float -- The payout you get for selling an item for the given price.
+    """
     return round(price - getTotalFeeFromPrice(price), 2)
 
 
 def getPriceFromPayout(payout):
+    """Returns the price you have to list an item for on the steam market to
+    get the given payout.
+    
+    Arguments:
+        payout {float} -- The payout you want to get.
+    
+    Returns:
+        float -- The price you have to list an item for on the steam market to
+            get the given payout.
+    """
     return round(payout + getTotalFeeFromPayout(payout), 2)
 
 
 def getTotalFeeFromPrice(price):
+    """Returns the fee you have to pay if you sell an item on the steam market for
+    given price.
+    
+    Arguments:
+        price {float} -- The price the buyer pays to buy your item.
+    
+    Returns:
+        float -- The fee you have to pay for selling an item on the steam market for
+            the given price.
+    """
     for i in reversed(range(0, int(price * 100), 1)):
         current_payout = i/100
         if round(getTotalFeeFromPayout(current_payout) + current_payout, 2) == round(price, 2):
@@ -22,6 +51,16 @@ def getTotalFeeFromPrice(price):
 
 
 def getTotalFeeFromPayout(payout):
+    """Returns the fee you have to pay if you sell an item on the steam market for
+    given payout.
+    
+    Arguments:
+        payout {float} -- The payout you get for selling an item.
+    
+    Returns:
+        float -- The fee you have to pay for selling an item on the steam market for
+            the given payout.
+    """
     steam_transaction_fee = math.floor((payout * STEAM_FEE) * 100) / 100
     csgo_transaction_fee = math.floor((payout * CSGO_FEE) * 100) / 100
     if steam_transaction_fee == 0:
@@ -32,6 +71,16 @@ def getTotalFeeFromPayout(payout):
 
 
 def getProfit(buyPrice, units, sellPrice):
+    """Returns the profit for selling a given amount of units.
+    
+    Arguments:
+        buyPrice {flaot} -- The price you payed per item.
+        units {int} -- The amount of items you are selling.
+        sellPrice {float} -- The price the buyer pays for your items.
+    
+    Returns:
+        float -- The profit.
+    """
     investment = buyPrice * units
     profit = units * getPayoutFromPrice(sellPrice) - investment
     return profit
